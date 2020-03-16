d44fe05418ff992385a9c8d80066b9fa3287b201
钟海 <417233956@qq.com>
2020-03-16 19:19:06 +0800

    更新关系线及拖拽事件

--- src/module/relLine.js
+++ src/module/relLine.js
@@ -11,7 +11,9 @@ define(function(require, exports, module) {
     Module.register('relLine', function() {
         //创建关系线
         //关系线参数设置
-        var relLineOption = { color: '#009143', size: 1.5, dash: true, endPlugSize: 2, startSocket: "right", Socket: "left" };
+        // var relLineOption = { color: '#009143', size: 1.5, dash: true, endPlugSize: 2, startSocket: "right", Socket: "left" };
+        var relLineOption = { color: '#009143', size: 1.5, dash: false, endPlugSize: 2, startSocket: "right", Socket: "left" };
+        var startDraggable, endDraggable;
 
         function createRelLine(startNode, endNode) {
             var startElementId = startNode.rc.getId();
@@ -22,45 +24,51 @@ define(function(require, exports, module) {
             // var startElement = document.getElementById(km.getSelectedNode().rc.getId());
             //startPlug: 'disc', endPlug: 'disc'
             var line = new LeaderLine(startElement, endElement, relLineOption);
+            line.start = LeaderLine.pointAnchor(startElement, { x: startElement.getBBox().width / 2.0, y: 0 });
+            line.end = LeaderLine.pointAnchor(endElement, { x: endElement.getBBox() / 2, y: 0 });
             km._relLine_obj.push(line);
             return line;
         }
 
         function createCircleFun(line) {
-            var svg = km.getRenderContainer().container.node,
-                p0 = line.start,
+            removeCircleFun();
+            var line_box_length = 11;
+            var lineData = km._relLine[km._relLine_obj.indexOf(line)];
+            // var svg = km.getRenderContainer().container.node;
+            var svg = km.getPaper().getNode();
+            var p0 = km.getNodeById(lineData.startId).rc.node,
                 p1,
                 p2,
-                p3 = line.end;
+                p3 = km.getNodeById(lineData.endId).rc.node;
 
             p1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
             if (p1) {
-                p1.setAttribute("cx", p0.getBoundingClientRect().x);
-                p1.setAttribute("cy", p0.getBoundingClientRect().y);
+                p1.setAttribute("cx", p0.getBoundingClientRect().x + (p0.getBBox().width / 2 - line_box_length));
+                p1.setAttribute("cy", p0.getBoundingClientRect().y - line_box_length);
                 p1.setAttribute("r", 10);
                 p1.setAttribute("class", "rel-line-point-start");
-                p1.setAttribute("style", "-webkit-tap-highlight-color: transparent; box-shadow: transparent 0px 0px 1px; cursor: grab; user-select: none;fill: indianred;");
+                p1.setAttribute("style", "-webkit-tap-highlight-color: transparent; box-shadow: transparent 0px 0px 1px; cursor: grab; user-select: none;fill: #009143;"); //indianred
                 svg.appendChild(p1);
             }
 
             p2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
             if (p2) {
-                p2.setAttribute("cx", p3.getBoundingClientRect().x);
-                p2.setAttribute("cy", p3.getBoundingClientRect().y);
+                p2.setAttribute("cx", p3.getBoundingClientRect().x + (p3.getBBox().width / 2 - line_box_length));
+                p2.setAttribute("cy", p3.getBoundingClientRect().y - line_box_length);
                 p2.setAttribute("r", 10);
                 p2.setAttribute("class", "rel-line-point-end");
-                p2.setAttribute("style", "-webkit-tap-highlight-color: transparent; box-shadow: transparent 0px 0px 1px; cursor: grab; user-select: none;fill: indianred;");
+                p2.setAttribute("style", "-webkit-tap-highlight-color: transparent; box-shadow: transparent 0px 0px 1px; cursor: grab; user-select: none;fill: #009143;");
                 svg.appendChild(p2);
             }
-            var line_box_length = 8;
+
             var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
             if (line1) {
-                line1.setAttribute("x1", p0.getBoundingClientRect().x + (p0.getBoundingClientRect().width - line_box_length) / 2);
-                line1.setAttribute("y1", p0.getBoundingClientRect().y - (p0.getBoundingClientRect().height - line_box_length) / 2);
+                line1.setAttribute("x1", p1.getBoundingClientRect().x);
+                line1.setAttribute("y1", p1.getBoundingClientRect().y);
                 line1.setAttribute("x2", p1.getBoundingClientRect().x);
                 line1.setAttribute("y2", p1.getBoundingClientRect().y);
                 line1.setAttribute("class", "rel-line-line-start");
-                line1.setAttribute("style", "stroke: indianred;stroke-width: 2px;");
+                line1.setAttribute("style", "stroke: #009143;stroke-width: 2px;");
                 svg.appendChild(line1);
             }
 
@@ -69,10 +77,10 @@ define(function(require, exports, module) {
             if (line2) {
                 line2.setAttribute("x1", p2.getBoundingClientRect().x);
                 line2.setAttribute("y1", p2.getBoundingClientRect().y);
-                line2.setAttribute("x2", p3.getBoundingClientRect().x + (p3.getBoundingClientRect().width - line_box_length * 2) / 2);
-                line2.setAttribute("y2", p3.getBoundingClientRect().y - (p3.getBoundingClientRect().height - line_box_length * 2) / 2);
+                line2.setAttribute("x2", p2.getBoundingClientRect().x);
+                line2.setAttribute("y2", p2.getBoundingClientRect().y);
                 line2.setAttribute("class", "rel-line-line-end");
-                line2.setAttribute("style", "stroke: indianred;stroke-width: 2px;");
+                line2.setAttribute("style", "stroke: #009143;stroke-width: 2px;");
                 svg.appendChild(line2);
             }
 
@@ -98,34 +106,83 @@ define(function(require, exports, module) {
 
                 var point_start = { x: _p1.x - xy.p1.x, y: _p1.y - xy.p1.y };
                 var point_end = { x: _p2.x - xy.p2.x, y: _p2.y - xy.p2.y };
+
+                //存入数据
+                lineData.ctrl = [{
+                    "x": point_start.x,
+                    "y": point_start.y
+                }, {
+                    "x": point_end.x,
+                    "y": point_end.y
+                }];
+
                 line.setOptions({
                     startSocketGravity: [point_start.x, point_start.y],
                     endSocketGravity: [point_end.x, point_end.y]
                 });
+                startDraggable.position();
+                endDraggable.position();
             }
-            new PlainDraggable(p1, {
+            startDraggable = new PlainDraggable(p1, {
                 containment: field,
                 onMove: () => { update(); }
             });
-            new PlainDraggable(p2, {
+            endDraggable = new PlainDraggable(p2, {
                 containment: field,
                 onMove: () => { update(); }
             });
-            update();
+            // update();
 
         }
 
         function removeCircleFun() {
+            Array.prototype.forEach.call(document.getElementsByClassName("rel-line-point-start"), (item) => {
+                item.remove();
+            });
+            Array.prototype.forEach.call(document.getElementsByClassName("rel-line-point-end"), (item) => {
+                item.remove();
+            });
+            Array.prototype.forEach.call(document.getElementsByClassName("rel-line-line-start"), (item) => {
+                item.remove();
+            });
+            Array.prototype.forEach.call(document.getElementsByClassName("rel-line-line-end"), (item) => {
+                item.remove();
+            });
+            startDraggable = null;
+            endDraggable = null;
+        }
+
+        function updateCircleFun(line) {
+            createCircleFun(line);
+            updateCirclePosition(line);
+        }
+
+        function updateCirclePosition(line) {
+            var lineData = km._relLine[km._relLine_obj.indexOf(line)];
+
             var _p1 = document.getElementsByClassName("rel-line-point-start")[0];
             var _p2 = document.getElementsByClassName("rel-line-point-end")[0];
+            if (_p1 && lineData.ctrl) {
+                _p1.cx.baseVal.value = _p1.cx.baseVal.value + lineData.ctrl[0].x;
+                _p1.cy.baseVal.value = _p1.cy.baseVal.value + lineData.ctrl[0].y;
 
-            var _line1 = document.getElementsByClassName("rel-line-line-start")[0];
-            var _line2 = document.getElementsByClassName("rel-line-line-end")[0];
+                _p2.cx.baseVal.value = _p2.cx.baseVal.value + lineData.ctrl[1].x;
+                _p2.cy.baseVal.value = _p2.cy.baseVal.value + lineData.ctrl[1].y;
 
-            _p1.remove();
-            _p2.remove();
-            _line1.remove();
-            _line2.remove();
+                var _line1 = document.getElementsByClassName("rel-line-line-start")[0];
+                var _line2 = document.getElementsByClassName("rel-line-line-end")[0];
+                _line1.x2.baseVal.value = _p1.getBoundingClientRect().x;
+                _line1.y2.baseVal.value = _p1.getBoundingClientRect().y;
+                _line2.x1.baseVal.value = _p2.getBoundingClientRect().x;
+                _line2.y1.baseVal.value = _p2.getBoundingClientRect().y;
+
+                // line.setOptions({
+                //     startSocketGravity: [lineData.ctrl[0].x, lineData.ctrl[0].y],
+                //     endSocketGravity: [lineData.ctrl[1].x, lineData.ctrl[1].y]
+                // });
+                startDraggable.position();
+                endDraggable.position();
+            }
         }
         /**
          * @command Image
@@ -148,9 +205,18 @@ define(function(require, exports, module) {
                     var startElementId = curNode.rc.getId();
                     var startElement = document.getElementById(startElementId);
                     var newLine = new LeaderLine(startElement, endNodeAreaAnchor, relLineOption);
+                    //起点定点到中间点                   
+                    // newLine.start = LeaderLine.pointAnchor(startElement, { x: 0, y: 0 });
+                    newLine.start = LeaderLine.pointAnchor(startElement, { x: startElement.getBBox().width / 2.0, y: 0 });
+                    km._relLine = km._relLine || [];
+                    var _relLine = {
+                        "id": utils.guid(),
+                        "startId": curNode.data.id || "",
+                        "endId": ""
+                    };
                     km._relLine_obj.push(newLine);
-
-                    km.circle = { create: createCircleFun, remove: removeCircleFun };
+                    //挂载拖拽事件
+                    km.circle = { create: updateCircleFun, remove: removeCircleFun };
 
                     //关系线指向终点函数
                     function relLineMouseFun() {
@@ -160,8 +226,13 @@ define(function(require, exports, module) {
                                 var __endNode = document.getElementById(_curNode.rc.getId());
                                 if (newLine.start != __endNode) {
                                     newLine.end = __endNode;
+                                    //终点定点到中间点
+                                    newLine.end = LeaderLine.pointAnchor(__endNode, { x: __endNode.getBBox() / 2, y: 0 });
                                     km.getRenderContainer().container.removeEventListener("mousedown", relLineMouseFun);
                                     km.getRenderContainer().container.removeEventListener("mousemove", relLineMouseMVFun);
+                                    //记录关系线数据
+                                    _relLine.endId = _curNode.data.id || "";
+                                    km._relLine.push(_relLine);
                                     //打点
                                     // createCircleFun(newLine);
                                 }
@@ -207,7 +278,7 @@ define(function(require, exports, module) {
             }
         });
 
-        // 图标的图形
+        // 关系线的加载
         var LeaderLineRenderer = kity.createClass('LeaderLineRenderer', {
             base: kity.Group,
 
@@ -221,7 +292,8 @@ define(function(require, exports, module) {
                     setTimeout(() => {
                         /*rel line*/
                         var relLineArray = km._relLine || [];
-                        km._relLine_obj = [];
+                        km._relLine_obj = km._relLine_obj || [];
+                        //挂载渲染关系线事件
                         km._relLine_render = (time, expand) => {
                             console.error = () => {};
                             time = time || 0
@@ -231,34 +303,42 @@ define(function(require, exports, module) {
                                     let curNode = km.getSelectedNode();
                                     let curNodeChild = curNode != undefined ? curNode.children : null;
                                     // 折叠/展开，需要隐藏/显示关系线
-                                    let expand_relLine_fun = function(nodeChilds, itemLine) {
+                                    let expand_relLine_fun = function(nodeChilds, itemLine, lineData) {
                                         //折叠/展开，需要隐藏/显示关系先
                                         nodeChilds.forEach((childItem) => {
-                                            let childEle = document.getElementById(childItem.rc.getId());
-                                            //子节点中是否有关系线
-                                            if (itemLine.start == childEle || itemLine.end == childEle) {
+                                            let childEleId = childItem.data.id;
+                                            let childEle = childItem.rc.node;
+                                            if (lineData.startId == childEleId || lineData.endId == childEleId) {
                                                 //子节点的显示状态
-                                                if (childEle.getAttribute("display") == "none") {
+                                                // if (childEle.getAttribute("display") == "none") 
+                                                //两个父级都有可以被折叠
+                                                if (km.getNodeById(lineData.startId).rc.node.getAttribute("display") == "none" || km.getNodeById(lineData.endId).rc.node.getAttribute("display") == "none") {
                                                     itemLine.hide();
+                                                    //更新拖拽渲染点
+                                                    if (document.getElementsByClassName("rel-line-point-start").length > 0) {
+                                                        removeCircleFun(itemLine);
+                                                    }
                                                 } else {
                                                     itemLine.show();
                                                 }
                                             }
                                             //递归
-                                            expand_relLine_fun(childItem.children, itemLine);
+                                            expand_relLine_fun(childItem.children, itemLine, lineData);
                                         });
                                     };
                                     //遍历关系线对象
-                                    km._relLine_obj.forEach((itemLine) => {
+                                    km._relLine_obj.forEach((itemLine, lineIndex) => {
                                         try {
                                             itemLine.position();
-                                        } catch (error) {
-
-                                        }
+                                        } catch (error) {}
 
                                         if (expand) {
                                             //折叠/展开触发
-                                            expand_relLine_fun(curNodeChild, itemLine);
+                                            expand_relLine_fun(curNodeChild, itemLine, km._relLine[lineIndex]);
+                                        }
+                                        //更新拖拽渲染点
+                                        if (document.getElementsByClassName("rel-line-point-start").length > 0) {
+                                            updateCircleFun(itemLine);
                                         }
                                     });
                                 };
@@ -353,22 +433,7 @@ define(function(require, exports, module) {
                     },
 
                     update: function(icon, node, box) {
-                        // var data = node.getData('relLine');
-                        // var spaceLeft = node.getStyle('space-left'),
-                        //     x, y;
 
-                        // // icon.setValue(data);
-                        // x = box.left - icon.width - spaceLeft;
-                        // y = -icon.height / 2;
-
-                        // icon.setTranslate(x, y);
-
-                        // return new kity.Box({
-                        //     x: x,
-                        //     y: y,
-                        //     width: icon.width,
-                        //     height: icon.height
-                        // });
                     }
                 })
             }
